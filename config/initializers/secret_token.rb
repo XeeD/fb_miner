a1a2ecfece6bb016d429fe509bf692dcba9d4305
XeeD <xeedus@gmail.com>
2013-06-18 10:51:15 +0200

    Initial commit - rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FbMiner::Application.config.secret_key_base = '27a3b7a203fdcd09d0c6d23637487f7e682bbd84339765a148844d30568b0ce26a8569bb951a4f737b1291c6b8f3bac57d10e8907bb015802962782ad9eca927'
